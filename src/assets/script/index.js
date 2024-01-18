b52a3baa237c4aafb07c5622c9e9b26926d48ce6
willoch <wilfried.lochungvu@gmail.com>
2024-01-18 23:11:35 +0100

    main functions finished

--- src/assets/script/index.js
+++ src/assets/script/index.js
@@ -1,25 +1,59 @@
 import { Html5QrcodeScanner } from "html5-qrcode";
 
-console.log('index.js');
-
 const scanner = new Html5QrcodeScanner('scanner' , {
     qrbox: {
         width: 300,
         height: 300
     },
-    fps: 20,
+    fps: 40,
+    videoConstraints: {
+         facingMode: {
+            exact: "environment"
+        }
+    }
 });
 
 scanner.render(success, error);
 
-function success(result) {
-    document.getElementById('result').innerHTML=`<h2> <a href='${result}'>ok ${result}</a></h2>`;
+function success(reference) {
+    if (localStorage.getItem(reference) == null) {
+        localStorage.setItem(reference,1);
+    } else {
+        let stock = parseInt(localStorage.getItem(reference));
+        localStorage.setItem(reference, stock+1);
+    }
+    document.getElementById('btnSection').setAttribute('reference',reference);
+    document.getElementById('result').textContent=`Référence : ${reference}`;
+    document.getElementById('count').textContent=`Compté : ${localStorage.getItem(reference)}`;
+    document.getElementById('link').src = `https://media.e.leclerc/${reference}_1`;
+    document.getElementById('error').textContent=``;
+    restart();
+}
+
+function error() {
+    document.getElementById('error').textContent=`Une erreur est survenue !`;
+    restart();
+}
+
+function restart() {
     scanner.clear();
-    document.getElementById('reader').remove();
+    scanner.render(success, error);
 }
 
-function error(err) {
-    console.log(err);
+document.querySelector('#add').addEventListener('click' , add);
+document.querySelector('#minus').addEventListener('click' , minus);
 
+function add() {
+    let reference = document.getElementById('btnSection').getAttribute('reference');
+    console.log(reference);
+    let stock = parseInt(localStorage.getItem(reference));
+    localStorage.setItem(reference,stock+1);
+    document.getElementById('count').textContent=`Compté : ${localStorage.getItem(reference)}`;
 }
 
+function minus() {
+    let reference = document.getElementById('btnSection').getAttribute('reference');
+    let stock = parseInt(localStorage.getItem(reference));
+    localStorage.setItem(reference,stock-1);
+    document.getElementById('count').textContent=`Compté : ${localStorage.getItem(reference)}`;
+}
