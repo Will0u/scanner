ee6c8da53bd652148d07ae12ea0e0c67a57e2061
willoch <wilfried.lochungvu@gmail.com>
2024-01-19 19:03:04 +0100

    added restart

--- src/assets/script/_scanEvent.js
+++ src/assets/script/_scanEvent.js
@@ -31,7 +31,7 @@ export function success(
     document.querySelector('#result').textContent =`Référence : ${reference}`;
     document.querySelector('#count').textContent =`Compté : ${localStorage.getItem(reference)}`;
     document.querySelector('#error').textContent ='';
-    scanner.clear();
+    restart();
 }
 
 
